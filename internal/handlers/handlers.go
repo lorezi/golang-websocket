05c65e01119883a0c4f74f35d186e9e4efcfad24
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-06-02 12:39:02 +0100

    adds websocke endpoint handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 )
 
 var views = jet.NewSet(
@@ -12,6 +13,20 @@ var views = jet.NewSet(
 	jet.InDevelopmentMode(),
 )
 
+var upgradeConn = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+// WSJsonResponse defines the response sent back from websocket
+type WSJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+
+// Home displays the home page
 func Home(w http.ResponseWriter, r *http.Request) {
 	err := renderPage(w, "home.jet", nil)
 	if err != nil {
@@ -20,6 +35,25 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// WSEndPoint upgrades connection to websocket
+func WSEndPoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConn.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Client connected to the endpoint")
+
+	res := WSJsonResponse{
+		Message: `<em><small>Connected to server</small><em>`,
+	}
+
+	err = ws.WriteJSON(res)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+// renderPage renders a jet template
 func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
 
 	view, err := views.GetTemplate(tmpl)
